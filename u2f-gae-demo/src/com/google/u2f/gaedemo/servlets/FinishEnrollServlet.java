9922a9f8a769b1ac1d60e44e541902be53388b1a
Dirk Balfanz <dirk@balfanz.net>
2014-05-15 09:22:34 -0700

    adding xsrf protection
    added XSRF protection in GAE demo app.

--- u2f-gae-demo/src/com/google/u2f/gaedemo/servlets/FinishEnrollServlet.java
+++ u2f-gae-demo/src/com/google/u2f/gaedemo/servlets/FinishEnrollServlet.java
@@ -7,10 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.u2f.U2FException;
 import com.google.u2f.gaedemo.storage.TokenStorageData;
+import com.google.u2f.server.DataStore;
 import com.google.u2f.server.U2FServer;
 import com.google.u2f.server.data.SecurityKeyData;
 import com.google.u2f.server.messages.RegistrationResponse;
@@ -19,16 +22,32 @@ import com.google.u2f.server.messages.RegistrationResponse;
 @Singleton
 public class FinishEnrollServlet extends HttpServlet {
 
+  private final UserService userService =  UserServiceFactory.getUserService();
   private final U2FServer u2fServer;
+  private final DataStore dataStore;
 
   @Inject
-  public FinishEnrollServlet(U2FServer u2fServer) {
+  public FinishEnrollServlet(U2FServer u2fServer, DataStore dataStore) {
     this.u2fServer = u2fServer;
+    this.dataStore = dataStore;
   }
 
   public void doPost(HttpServletRequest req, HttpServletResponse resp) 
       throws IOException, ServletException {
 
+    // Simple XSRF protection. We don't want users to be tricked into
+    // submitting other people's enrollment data. Here we're just checking 
+    // that it's the same user that also started the enrollment - you might
+    // want to do something more sophisticated.
+    String currentUser = userService.getCurrentUser().getUserId();
+    String expectedUser = dataStore
+        .getEnrollSessionData(req.getParameter("sessionId"))
+        .getAccountName();
+    if (!currentUser.equals(expectedUser)) {
+      throw new ServletException("Cross-site request prohibited");
+    }   
+
+    
     RegistrationResponse registrationResponse = new RegistrationResponse(
         req.getParameter("enrollData"), 
         req.getParameter("browserData"),
--- u2f-gae-demo/src/com/google/u2f/gaedemo/servlets/FinishSignServlet.java
+++ u2f-gae-demo/src/com/google/u2f/gaedemo/servlets/FinishSignServlet.java
@@ -7,10 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.u2f.U2FException;
 import com.google.u2f.gaedemo.storage.TokenStorageData;
+import com.google.u2f.server.DataStore;
 import com.google.u2f.server.U2FServer;
 import com.google.u2f.server.data.SecurityKeyData;
 import com.google.u2f.server.messages.SignResponse;
@@ -19,16 +22,32 @@ import com.google.u2f.server.messages.SignResponse;
 @Singleton
 public class FinishSignServlet extends HttpServlet {
 
+  private final UserService userService =  UserServiceFactory.getUserService();
   private final U2FServer u2fServer;
+  private final DataStore dataStore;
 
   @Inject
-  public FinishSignServlet(U2FServer u2fServer) {
+  public FinishSignServlet(U2FServer u2fServer, DataStore dataStore) {
     this.u2fServer = u2fServer;
+    this.dataStore = dataStore;
   }
 
   public void doPost(HttpServletRequest req, HttpServletResponse resp) 
       throws IOException, ServletException {
 
+    // Simple XSRF protection. We don't want users to be tricked into
+    // submitting other people's enrollment data. Here we're just checking 
+    // that it's the same user that also started the enrollment - you might
+    // want to do something more sophisticated.
+    String currentUser = userService.getCurrentUser().getUserId();
+    String expectedUser = dataStore
+        .getSignSessionData(req.getParameter("sessionId"))
+        .getAccountName();
+    if (!currentUser.equals(expectedUser)) {
+      throw new ServletException("Cross-site request prohibited");
+    }   
+    
+    
     SignResponse signResponse = new SignResponse(
         req.getParameter("browserData"),
         req.getParameter("signatureData"),

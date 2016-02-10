c5ddfc607d5a85099b03843501d8189ad7943848
Alexei Czeskis <aczeskis@google.com>
2016-02-09 23:35:20 -0800

    Parse and display block mode and key size

--- u2f-ref-code/java/tests/com/google/u2f/server/impl/attestation/android/AuthorizationListTest.java
+++ u2f-ref-code/java/tests/com/google/u2f/server/impl/attestation/android/AuthorizationListTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.security.cert.CertificateParsingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -26,26 +27,28 @@ public class AuthorizationListTest {
   private static final List<Purpose> ONE_PURPOSE = Arrays.asList(Purpose.KM_PURPOSE_SIGN);
   private static final List<Purpose> TWO_PURPOSES =
       Arrays.asList(Purpose.KM_PURPOSE_SIGN, Purpose.KM_PURPOSE_VERIFY);
+  private static final Integer KEY_SIZE = 256;
+  private static final List<BlockMode> EMPTY_BLOCKMODE = new ArrayList<BlockMode>();
+  private static final List<BlockMode> ONE_BLOCKMODE = Arrays.asList(BlockMode.KM_MODE_CBC);
+  private static final List<BlockMode> TWO_BLOCKMODE =
+      Arrays.asList(BlockMode.KM_MODE_CBC, BlockMode.KM_MODE_CTR);
 
   @Test
   public void toJson_nullValues() throws Exception {
-    JsonObject json = new AuthorizationList(null, null).toJson();
+    JsonObject json = new AuthorizationList(null, null, null, null).toJson();
 
     assertFalse(json.has(AuthorizationList.JSON_ALGORITHM_KEY));
     assertFalse(json.has(AuthorizationList.JSON_PURPOSE_KEY));
+    assertFalse(json.has(AuthorizationList.JSON_KEY_SIZE_KEY));
+    assertFalse(json.has(AuthorizationList.JSON_BLOCK_MODE_KEY));
   }
 
   @Test
   public void toJson_emptyPurpose() throws Exception {
-    AuthorizationList authorizationList =
-        new AuthorizationList(EMPTY_PURPOSE, Algorithm.KM_ALGORITHM_EC);
-    JsonObject json = authorizationList.toJson();
+    JsonObject json = new AuthorizationList(EMPTY_PURPOSE, null, null, null).toJson();
 
-    assertEquals(
-        Algorithm.KM_ALGORITHM_EC.toString(),
-        json.get(AuthorizationList.JSON_ALGORITHM_KEY).getAsString());
-    List<Purpose> extractedPurpose = extractPurposeListFromJsonArray(
-        json.get(AuthorizationList.JSON_PURPOSE_KEY).getAsJsonArray());
+    List<Purpose> extractedPurpose = extractListFromJsonArray(
+        json.get(AuthorizationList.JSON_PURPOSE_KEY).getAsJsonArray(), Purpose.class);
 
     assertTrue(EMPTY_PURPOSE.containsAll(extractedPurpose));
     assertTrue(extractedPurpose.containsAll(EMPTY_PURPOSE));
@@ -53,15 +56,11 @@ public class AuthorizationListTest {
 
   @Test
   public void toJson_onePurpose() throws Exception {
-    AuthorizationList authorizationList =
-        new AuthorizationList(ONE_PURPOSE, Algorithm.KM_ALGORITHM_HMAC);
+    AuthorizationList authorizationList = new AuthorizationList(ONE_PURPOSE, null, null, null);
     JsonObject json = authorizationList.toJson();
 
-    assertEquals(
-        Algorithm.KM_ALGORITHM_HMAC.toString(),
-        json.get(AuthorizationList.JSON_ALGORITHM_KEY).getAsString());
-    List<Purpose> extractedPurpose = extractPurposeListFromJsonArray(
-        json.get(AuthorizationList.JSON_PURPOSE_KEY).getAsJsonArray());
+    List<Purpose> extractedPurpose = extractListFromJsonArray(
+        json.get(AuthorizationList.JSON_PURPOSE_KEY).getAsJsonArray(), Purpose.class);
 
     assertTrue(ONE_PURPOSE.containsAll(extractedPurpose));
     assertTrue(extractedPurpose.containsAll(ONE_PURPOSE));
@@ -69,24 +68,87 @@ public class AuthorizationListTest {
 
   @Test
   public void toJson_twoPurposes() throws Exception {
-    JsonObject json = new AuthorizationList(TWO_PURPOSES, Algorithm.KM_ALGORITHM_RSA).toJson();
+    JsonObject json = new AuthorizationList(TWO_PURPOSES, null, null, null).toJson();
 
-    assertEquals(
-        Algorithm.KM_ALGORITHM_RSA.toString(),
-        json.get(AuthorizationList.JSON_ALGORITHM_KEY).getAsString());
-    List<Purpose> extractedPurpose = extractPurposeListFromJsonArray(
-        json.get(AuthorizationList.JSON_PURPOSE_KEY).getAsJsonArray());
+    List<Purpose> extractedPurpose = extractListFromJsonArray(
+        json.get(AuthorizationList.JSON_PURPOSE_KEY).getAsJsonArray(), Purpose.class);
 
     assertTrue(TWO_PURPOSES.containsAll(extractedPurpose));
     assertTrue(extractedPurpose.containsAll(TWO_PURPOSES));
   }
 
-  private List<Purpose> extractPurposeListFromJsonArray(JsonArray array) throws Exception {
+  @Test
+  public void toJson_algorithm() throws Exception {
+    JsonObject json = new AuthorizationList(null, Algorithm.KM_ALGORITHM_EC, null, null).toJson();
+
+    assertEquals(
+        Algorithm.KM_ALGORITHM_EC.toString(),
+        json.get(AuthorizationList.JSON_ALGORITHM_KEY).getAsString());
+  }
+
+  @Test
+  public void toJson_keysize() throws Exception {
+    JsonObject json = new AuthorizationList(null, null, KEY_SIZE, null).toJson();
+
+    assertEquals(KEY_SIZE.intValue(), json.get(AuthorizationList.JSON_KEY_SIZE_KEY).getAsInt());
+  }
+
+  @Test
+  public void toJson_emptyBlockMode() throws Exception {
+    JsonObject json = new AuthorizationList(null, null, null, EMPTY_BLOCKMODE).toJson();
+
+    List<BlockMode> extractedBlockMode = extractListFromJsonArray(
+        json.get(AuthorizationList.JSON_BLOCK_MODE_KEY).getAsJsonArray(), BlockMode.class);
+
+    assertTrue(EMPTY_BLOCKMODE.containsAll(extractedBlockMode));
+    assertTrue(extractedBlockMode.containsAll(EMPTY_BLOCKMODE));
+  }
+
+  @Test
+  public void toJson_oneBlockMode() throws Exception {
+    AuthorizationList authorizationList = new AuthorizationList(null, null, null, ONE_BLOCKMODE);
+    JsonObject json = authorizationList.toJson();
+
+    List<BlockMode> extractedBlockMode = extractListFromJsonArray(
+        json.get(AuthorizationList.JSON_BLOCK_MODE_KEY).getAsJsonArray(), BlockMode.class);
+
+    assertTrue(ONE_BLOCKMODE.containsAll(extractedBlockMode));
+    assertTrue(extractedBlockMode.containsAll(ONE_BLOCKMODE));
+  }
+
+  @Test
+  public void toJson_twoBlockMode() throws Exception {
+    JsonObject json = new AuthorizationList(null, null, null, TWO_BLOCKMODE).toJson();
+
+    List<BlockMode> extractedBlockMode = extractListFromJsonArray(
+        json.get(AuthorizationList.JSON_BLOCK_MODE_KEY).getAsJsonArray(), BlockMode.class);
+
+    assertTrue(TWO_BLOCKMODE.containsAll(extractedBlockMode));
+    assertTrue(extractedBlockMode.containsAll(TWO_BLOCKMODE));
+  }
+
+  // TODO(aczeskis): There is a cleaner way of doing this in Java 8.  In Java 8, we can make Purpose
+  // & BlockMode implement an interface (so the function could call .fromString() on the
+  // parameterized type).  Unfortunately, Java 7 does not allow interfaces to have static methods!
+  // This decision was fixed in Java 8.
+  private <T> List<T> extractListFromJsonArray(JsonArray array, Class<T> type) throws Exception {
     Iterator<JsonElement> iterator = array.iterator();
-    List<Purpose> result = new ArrayList<Purpose>();
+    List<T> result = new ArrayList<T>();
     while (iterator.hasNext()) {
-      result.add(Purpose.fromString(iterator.next().getAsString()));
+      result.add(buildTypeFromString(iterator.next().getAsString(), type));
     }
     return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T> T buildTypeFromString(String string, Class<T> type)
+      throws CertificateParsingException {
+    if (type == Purpose.class) {
+      return (T) Purpose.fromString(string);
+    } else if (type == BlockMode.class) {
+      return (T) BlockMode.fromString(string);
+    } else {
+      throw new CertificateParsingException("Cannot build type " + type.getSimpleName());
+    }
   }
 }
